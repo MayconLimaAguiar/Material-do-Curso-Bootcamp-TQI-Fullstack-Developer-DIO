8074ad1d77a9b17c7bd2fdd99a717b28092ae5cf
Maycon <mayconl.aguiar@gmail.com>
2022-06-15 21:46:52 +0044

    adiciona mais dois exercicios de JS

---
+++ Atividade-JS/idade.js
@@ -0,0 +1,23 @@
+function calculeIdade(anos) {
+    return `Daqui a ${anos} anos, ${this.nome} terá ${
+        this.idade + anos
+    } anos de idade.`;
+}
+
+const pessoa1 = {
+    nome: 'Maria',
+    idade: 30
+};
+
+const pessoa2 = {
+    nome: 'Carla',
+    idade: 26,
+};
+
+const animal = {
+    nome: 'Fiona',
+    idade: 5,
+    raca: 'pug',
+};
+
+console.log(calculeIdade.call(pessoa2, 30));
